98b7090ddf9d617c5ab57c642abb04fc92a6008d
Matias Iturralde <matiasiturralde@Matiass-MacBook-Air.local>
2021-07-01 18:14:36 -0300

    Profe 09

--- profesores.py
+++ profesores.py
@@ -4,14 +4,19 @@ import pandas as pd
 import streamlit as st
 import io
 
+favicon = 'https://res.cloudinary.com/hdsqazxtw/image/upload/v1559681445/logo_coderhouse_1_rec5vl.png'
+st.set_page_config(page_title='Profesores', page_icon = favicon, initial_sidebar_state = 'auto', layout="centered")
+# favicon being an object of the same kind as the one you should provide st.image() with (ie. a PIL array for example) or a string (url or local file path)
+
 col1, col2, col3 = st.beta_columns(3)
 image = 'https://res.cloudinary.com/hdsqazxtw/image/upload/v1559681445/logo_coderhouse_3_bllxal.png'
 col2.image(image)
 
-st.markdown("""
+markdown = st.markdown("""
 <style>
 .big-font {
     font-size:20px !important;
+    text-align: left
 }
 </style>
 """, unsafe_allow_html=True)
@@ -20,12 +25,17 @@ st.markdown('<p class="big-font">Liquidación Profesores</p>', unsafe_allow_html
 
 #st.title('Liquidación Profesores:')
 #st.text('Cargar archivo de liquidación mensual:')
-#st.sidebar
+#st.sidebarS
 
-multiple_files = st.file_uploader('Subir csv',type="csv", accept_multiple_files=True)
-for file in multiple_files:
-	dataframe = pd.read_csv(file)
-	file.seek(0)
-	st.write(dataframe)
-    
-st.write("###")
+multiple_files = st.file_uploader('Subir csv:',type="csv", accept_multiple_files=True)
+if multiple_files is not None:
+    for file in multiple_files:
+	    df = pd.read_csv(file)
+	    st.dataframe(df)
+        
+if multiple_files is not None:
+    for file in multiple_files:
+        df['monto total'] = df['monto total'].fillna(0)
+        df['monto total'] = df['monto total'].str.replace('-','0')
+        df['monto total'] = df['monto total'].str.replace(',','.').astype('float')
+        st.write('Monto Total:' , "ARS {0:,.2f}".format(df['monto total'].sum()))
