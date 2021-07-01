41e6951010ecc8bedf92a088ea75ef2ecf2ce463
Matias Iturralde <matiasiturralde@Matiass-MacBook-Air.local>
2021-06-30 23:25:39 -0300

    Profe 02

--- profesores.py
+++ profesores.py
@@ -3,36 +3,35 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import io
 
-"""
-# Welcome to Streamlit!
+col1, col2, col3 = st.beta_columns(3)
+image = 'https://res.cloudinary.com/hdsqazxtw/image/upload/f_auto/v1579032128/coder-logo-white_t41xzb.png'
+col2.image(image)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.markdown("""
+<style>
+.big-font {
+    font-size:20px !important;
+}
+</style>
+""", unsafe_allow_html=True)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.markdown('<p class="big-font">Liquidación Profesores</p>', unsafe_allow_html=True)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+#st.title('Liquidación Profesores:')
+#st.text('Cargar archivo de liquidación mensual:')
+#st.sidebar
 
+multiple_files = st.file_uploader(
+    'Subir csv:',
+    accept_multiple_files=True
+)
+for file in multiple_files:
+    file_container = st.beta_expander(
+        f"File name: {file.name} ({file.size})"
+    )
+    data = io.BytesIO(file.getbuffer())
+    file_container.write(pd.read_csv(data))
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.write("### Code")
